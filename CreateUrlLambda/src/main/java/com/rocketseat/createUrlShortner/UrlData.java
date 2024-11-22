b5c032de1281378e3b2f57f59cdf25cd01a5d629
Ricardo Ribeiro <102837467+MatosRicardo@users.noreply.github.com>
2024-11-22 17:03:40 -0300

    Aula 2 Finalizada

---
+++ CreateUrlLambda/src/main/java/com/rocketseat/createUrlShortner/UrlData.java
@@ -0,0 +1,29 @@
+package com.rocketseat.createUrlShortner;
+
+public class UrlData {
+    private String originalUrl;
+    private long expirationTimeInSeconds;
+
+    public UrlData(String originalUrl, long expirationTimeInSeconds) {
+        this.originalUrl = originalUrl;
+        this.expirationTimeInSeconds = expirationTimeInSeconds;
+    }
+
+    // Getters e Setters
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public long getExpirationTimeInSeconds() {
+        return expirationTimeInSeconds;
+    }
+
+    public void setExpirationTimeInSeconds(long expirationTimeInSeconds) {
+        this.expirationTimeInSeconds = expirationTimeInSeconds;
+    }
+}
